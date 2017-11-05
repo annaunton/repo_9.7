36e670ea33ced50fe603aaca665423249f374d39
ani <anna.unton@yahoo.com>
2017-11-05 23:38:24 +0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
 // scripts.js
+var a = prompt('Enter variable a');
+var b = prompt('Enter variable b');
+var value = (a * a) - (2 * a * b) + (b * b);
+console.log(value);
 
-var a = prompt('Enter triangle base');
-var h = prompt('Enter triangle height');
-var triangleArea = a*h/2;
-alert('Triangle field with base ' + a + ' and height ' + h + ' is equal to ' + triangleArea);
-console.log('Triangle field with base ' + a + ' and height ' + h + ' is equal to ' + triangleArea);
+if (value > 0) {
+	console.log('Wynik dodatni');
+} else if (value < 0) {
+	console.log('Wynik ujemny');
+} else {
+	console.log('Wynik równy zero');
+}
