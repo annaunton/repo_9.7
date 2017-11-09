10e946dcdcd3dff1d2048ec8b7b1ea23ed42c74d
ani <anna.unton@yahoo.com>
2017-11-10 03:03:59 +0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 // scripts.js
-var a = prompt('Enter variable a');
-var b = prompt('Enter variable b');
-var value = (a * a) - (2 * a * b) + (b * b);
-console.log(value);
 
-if (value > 0) {
-	console.log('Wynik dodatni');
-} else if (value < 0) {
-	console.log('Wynik ujemny');
-} else {
-	console.log('Wynik równy zero');
+var triangleBase = prompt('Enter triangle base');
+var triangleHeight = prompt('Enter triangle height');
+
+function getTriangleArea(a,h) {
+	if (a <= 0 || h <= 0) {
+		console.log('Błędne dane');
+	} else {
+		var result = a*h/2;
+		return result;	
+	}
 }
+
+var triangle1Area = getTriangleArea(triangleBase,triangleHeight);
+var triangle2Area = getTriangleArea(8,5);
+var triangle3Area = getTriangleArea(100,0.5);
+
+console.log(triangle1Area);
+console.log(triangle2Area);
+console.log(triangle3Area);
