36a29abe1af82a1f56d43a6e9a52e7d72abb153d
ani <anna.unton@yahoo.com>
2017-11-12 05:00:19 +0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,10 @@
 // scripts.js
 
-var height = prompt('Jak wysoką chcesz mieć w tym roku choinkę? Podaj liczbę poziomów.');
+var buttons = document.getElementsByClassName('button');
 
 
-function drawTree(height) {
-	var star ='';
-	for (var i=1; i<=height; i++) {
-		star += '*';	
-		console.log(star);
-	}
+for (var i=0; i <= buttons.length-1; i++) {
+	console.log(buttons[i].innerHTML);
+
 }
-drawTree(height);
 
-
-/* Poniżej próbowałam zrobić według instrukcji z modułu, ale wyszły jakieś bzdury. Możesz zobaczyć, co jest źle? Po co wprowadzać dwie pętle? Co to daje?*/
-
-
-/*
- var height = prompt('Jak wysoką chcesz mieć w tym roku choinkę? Podaj liczbę poziomów.');
-
-
-function drawTree(height) {
-
-	for (var i=0; i<=height; i++) {
-		star = '';	
-		for (var j=0; i<=j; j++) {
-			star += '*';	
-		}
-		console.log(star);
-	}
-}
-drawTree(height);*/
