14f952ca2cebc230859932bdc18d524862a153eb
ani <anna.unton@yahoo.com>
2017-11-11 22:38:51 +0700

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,33 @@
 // scripts.js
 
-var text = 'Velociraptor is a genus of herbivorous ceratopsid dinosaur that first appeared during the late Maastrichtian stage of the late Cretaceous period.';
-
-var dinosaur = 'triceratops';
-
-var newText = text.replace('Velociraptor', dinosaur.toUpperCase());
-
-console.log(newText.slice(0,newText.length/2));
+var height = prompt('Jak wysoką chcesz mieć w tym roku choinkę? Podaj liczbę poziomów.');
 
 
+function drawTree(height) {
+	var star ='';
+	for (var i=1; i<=height; i++) {
+		star += '*';	
+		console.log(star);
+	}
+}
+drawTree(height);
 
 
 
+
+/* Według instrukcji  z modułu*/
+/*
+ var height = prompt('Jak wysoką chcesz mieć w tym roku choinkę? Podaj liczbę poziomów.');
+
+
+function drawTree(height) {
+
+	for (var i=0; i<=height; i++) {
+		star = '';	
+		for (var j=0; i<=j; j++) {
+			star += '*';	
+		}
+		console.log(star);
+	}
+}
+drawTree(height);*/
